671f5707440a0cebde813ac96d992064b3dc2c16
Long Le <philong6297@gmail.com>
2017-12-15 02:09:09 +0700

    update The Core

---
+++ Arcade/The Core/Intro Gates/addTwoDigits.cpp
@@ -0,0 +1,13 @@
+/*
+You are given a two-digit integer n. Return the sum of its digits.
+
+Example
+
+For n = 29, the output should be
+addTwoDigits(n) = 11.
+*/
+int addTwoDigits(int n) {
+	int sum = 0;
+	while (n) sum += n % 10, n /= 10;
+	return sum;
+}
---
+++ Arcade/The Core/Intro Gates/largestNumber.cpp
@@ -0,0 +1,11 @@
+/*
+Given an integer n, return the largest number that contains exactly n digits.
+
+Example
+
+For n = 2, the output should be
+largestNumber(n) = 99.
+*/
+int largestNumber(int n) {
+	return pow(10, n) - 1;
+}
